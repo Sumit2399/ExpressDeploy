39c33492c6c111abffdf7109cd3da378dcf019c4
Sumit2399 <sumit2399@gmail.com>
2023-05-28 15:19:48 +0530

    deploy

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 const fs=require('fs')
+const path=require('path')
 const i=fs.readFileSync('index.html','utf-8')
-const i1=JSON.parse(fs.readFileSync('data.json','utf-8'))
+const i1=JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'),'utf-8'))
 const users=i1.users;
 
 
