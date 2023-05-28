ee919add110da50ac5ace5642b0ea6aab28864ec
Sumit2399 <sumit2399@gmail.com>
2023-05-28 15:49:29 +0530

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express=require('express')
 const server=express();
-const morgan=require('morgan')
+
 const mongoose=require('mongoose')
 const cors=require('cors')
 const path=require('path')
